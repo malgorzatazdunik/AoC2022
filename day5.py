94d7052a4bb3eb179a3c16c004791bc0c819a046
goszk <g.zdunik@gmail.com>
2022-12-21 18:21:04 +0000

    fixed

--- day5.py
+++ day5.py
@@ -26,35 +26,50 @@ def get_stacks_and_instructions(_data):
     _n_stacks = max([int(i) for i in _stacks[-1].strip().split("  ")])
     _stacks = [stack[1:-1:4] for stack in _stacks[:-1]]
 
-    _instructions = _data[i + 1:]
-    return _n_stacks, _stacks, _instructions
-
-
-def move_elements_in_stacks(_n_stacks, _stacks, _instructions):
-    # n_stacks = max([int(i) for i in _stacks[-1].strip().split("  ")]) - 1
-
-    l_stacks = [[] for _ in range(_n_stacks)]
+    stacks = [[] for _ in range(_n_stacks)]
     for row in _stacks:
         i = 0
         for char in row:
             if char != ' ':
-                l_stacks[i].append(char)
+                stacks[i].append(char)
             i += 1
 
-    l_stacks = [stack[::-1] for stack in l_stacks]
+    stacks = [stack[::-1] for stack in stacks]
 
-    nums = []
+    _instructions = _data[i + 1:]
+
+    instructions = []
     for instruction in _instructions:
-        a, b, c = [int(i) for i in instruction.replace("move", "").replace("from", "").replace("to", "").strip().split("  ")]
-        nums.append([a, b, c])
+        a, b, c = [int(i) for i in
+                   instruction.replace("move", "").replace("from", "").replace("to", "").strip().split("  ")]
+        instructions.append([a, b, c])
+    return stacks, instructions
 
-    for n in nums:
+
+def move_elements_in_stacks(stacks, instructions):
+
+    for n in instructions:
         for el in range(0, n[0]):
-            l_stacks[n[-1]-1].append(l_stacks[n[1]-1].pop())
+            stacks[n[-1]-1].append(stacks[n[1]-1].pop())
 
-    return l_stacks
+    return stacks
 
 
-n_stacks, stacks, instructions = get_stacks_and_instructions(data)
-final_stacks = move_elements_in_stacks(n_stacks, stacks, instructions)
-print("".join([stack.pop() for stack in final_stacks]))
+def move_elements_in_stacks_2(stacks, instructions):
+
+    for n in instructions:
+        removed = []
+        for el in range(0, n[0]):
+            removed.append(stacks[n[1]-1].pop())
+        stacks[n[-1] - 1].extend(removed[::-1])
+
+    return stacks
+
+
+stacks, instructions = get_stacks_and_instructions(data)
+final_stacks_1 = move_elements_in_stacks(stacks, instructions)
+print("".join([stack.pop() for stack in final_stacks_1]))
+
+stacks, instructions = get_stacks_and_instructions(data)
+final_stacks_2 = move_elements_in_stacks_2(stacks, instructions)
+print("".join([stack.pop() for stack in final_stacks_2]))
